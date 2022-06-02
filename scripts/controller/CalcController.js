456809f1a5729f95809e547fd0518ec8b49a906e
Willames Santos <willamessantos@live.com>
2022-06-01 22:41:45 -0300

    Adicionando hora e data automática em tempo real

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,33 +2,66 @@ class CalcController {
 
     constructor(){
 
-        this._displayCalc = "0";
-        this._dataAtual;
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEL = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
 
+    }
+
+    initialize(){
+        
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+
+            this.setDisplayDateTime();
+
+        }, 1000)
+        
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayDate(){
+        return this._dateEL.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEL.innerHTML = value;
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
     }
 
     get displayCalc(){
-
-        return this._displayCalc;
-
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-
-        this._displayCalc = valor;
-
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual(){
-
-        return this._dataAtual;
-
+    get currentDate(){
+        return new Date();
     }
 
-    set dataAtual(valor){
-
-        this._dataAtual = valor;
-
+    set currentDate(value){
+        this._currentDate = value;
     }
 
 }
